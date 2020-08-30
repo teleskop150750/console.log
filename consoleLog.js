3fc42527bc24ac8d819e133d3d629f62a0c10200
teleskop 150750 <teleskop150750@gmail.com>
2020-08-30 12:13:38 +0300

    отображение объектов

---
+++ consoleLog.js
@@ -0,0 +1,40 @@
+const template = (resultData) => {
+  let resultStrig = '';
+
+  resultData.forEach((el) => {
+    if (Array.isArray(el)) {
+      resultStrig += `[${el.join(', ')}] `;
+    } else if (typeof el == 'object') {
+      const entries = Object.entries(el).reduce((acc, [key, value]) => acc + `  ${key}: ${value},\n`, '');
+      resultStrig += `\n{\n${entries}}\n`;
+    } else {
+      resultStrig += `${el} `;
+    }
+  });
+  resultStrig = resultStrig.trim();
+
+  const backgroundColor = document.querySelectorAll('.span-result-wrapper').length % 2 === 0 ? '#bee7c4' : '#bee2e7';
+
+  const spanWrapper = document.createElement('div');
+  spanWrapper.style.cssText = `border-bottom: 1px solid #c0c0c0;
+  background-color: ${backgroundColor}`;
+  spanWrapper.classList.add('span-result-wrapper');
+
+  const span = document.createElement('span');
+  span.textContent = resultData.join(' ');
+  span.textContent = resultStrig;
+  span.style.cssText = `display: inline-block;
+  padding: 6px 10px;
+  white-space: pre-wrap;`;
+  span.classList.add('span-result');
+
+  spanWrapper.append(span);
+  return spanWrapper;
+};
+
+const consoleLog = (parent) => (...resultData) => {
+  const result = template(resultData);
+  parent.append(result);
+};
+
+export default consoleLog;
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-import printResult from './printResult.js';
+import printResult from './consoleLog.js';
 
-const body = document.querySelector('.resuts');
-
+const body = document.querySelector('.console');
 const print = printResult(body);
+
+print(1234, 'текст')
+
 const arr = [1, 3, 5, 6];
-const d = () => 4 + 5;
-print(d());
-print(3 + 4);
-print('3 + 4');
+
 print(arr);
 print(...arr);
+print(42, arr, 42);
+print(42, ...arr, 42);
+
+const sum = (a = 2) => a ** 2;
+
+print(sum);
+print(sum(3))
+
+const obj = {
+  name: 'Иван',
+  sayHi() {
+    return `Привет ${this.name}`;
+  },
+}
+print(obj);
